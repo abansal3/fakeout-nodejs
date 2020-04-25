d49846512026c30be33e69dce5c3341ebd50a5a6
Arpit Bansal <saas@arpitbansal.com>
2020-04-26 00:41:57 +0800

    Product complete for demo

--- routes/flag.js
+++ routes/flag.js
@@ -30,4 +30,46 @@ router.post('/flag', function(req, res, next) {
       });
 });
 
+router.post('/check', function(req, res, next) {
+    Flag.findOne({ linkUrl: req.body.url }, (err, existingFlag) => {
+        if (err) { return next(err); }
+
+        if (existingFlag) {
+            console.log("Flagged url found");
+            res.json({flagged: true})
+        } else {
+            console.error('No flagged link found');
+            res.send('No flagged link found');
+        }
+    });
+});
+
+router.post('/checkIfAlreadyFlaggedByUser', function(req, res, next) {
+    Flag.findOne({ linkUrl: req.body.link, flaggedBy: req.body.user }, (err, existingFlag) => {
+        if (err) { return next(err); }
+
+        if (existingFlag) {
+            console.log("Flagged url found by this user");
+            res.json({flaggedByUser: true})
+        } else {
+            console.error('No flagged link found');
+            res.send('No flagged link found');
+        }
+    });
+});
+
+router.post('/getFlagsByUser', function(req, res, next) {
+    Flag.find({ flaggedBy: req.body.user }, (err, flags) => {
+        if (err) { return next(err); }
+
+        if (flags) {
+            console.log("Flags found for user");
+            res.json({flags: flags});
+        } else {
+            console.error('No flagged link found');
+            res.send('No flagged link found');
+        }
+    }).limit(5);
+});
+
 module.exports = router;
